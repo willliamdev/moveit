38fe97e5b08295da0e38fec73eb8d39619275877
William almeida <willliam.dev@gmail.com>
2022-04-10 23:43:55 -0300

    add title to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import styles from '../styles/pages/Home.module.css'
@@ -8,11 +10,15 @@ import styles from '../styles/pages/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Moveit</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
           <p>sadf</p>
