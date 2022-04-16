3580dbc8b93a24ff886f9a2a927b66aa5c685656
William almeida <willliam.dev@gmail.com>
2022-04-16 19:16:14 -0300

    create a context to challenges data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { ChallengesProvider } from '../contexts/ChallengesContext'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
