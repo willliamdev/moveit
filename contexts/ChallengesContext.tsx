f730630bd5cb3ed145c6b1e41428c3fc56ca2282
William almeida <willliam.dev@gmail.com>
2022-04-17 23:35:29 -0300

    sharing challenge with components

--- contexts/ChallengesContext.tsx
+++ contexts/ChallengesContext.tsx
@@ -1,19 +1,58 @@
 import { createContext, ReactNode, useState } from 'react'
+import challenges from '../challenges.json'
 
-export const ChallengesContext = createContext({})
+interface Challenge {
+  type: "body" | "eye";
+  description: string
+  amount: number
+}
+
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge | null;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
 
 interface ChallengesProviderProps {
   children: ReactNode;
-
 }
+
+export const ChallengesContext = createContext({} as ChallengesContextData)
+
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(0)
+  const [currentExperience, setCurrentExperience] = useState(0)
+  const [challengesCompleted, setChallengesCompleted] = useState(0)
+  // bug to fix: initialize as null cause errror with type
+  const [activeChallenge, setActiveChallenge] = useState(null)
+
 
   function levelUp() {
     setLevel(level + 1)
   }
+
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+    const challenge = challenges[randomChallengeIndex]
+    setActiveChallenge(challenge)
+  }
+
   return (
-    < ChallengesContext.Provider value={{ level, levelUp }} >
+    < ChallengesContext.Provider
+      value={
+        {
+          level,
+          currentExperience,
+          challengesCompleted,
+          activeChallenge,
+          levelUp,
+          startNewChallenge
+        }
+      }
+    >
       {children}
     </ChallengesContext.Provider >
   )
