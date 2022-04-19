9d88baad9008f435c067968409b037e031e05fa8
William almeida <willliam.dev@gmail.com>
2022-04-19 15:44:25 -0300

    cleaning  the component and using the context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { ChallengesProvider } from '../contexts/ChallengesContext'
+import { CountdownProvider } from '../contexts/CountdownContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChallengesProvider>
-      <Component {...pageProps} />
+      <CountdownProvider>
+        <Component {...pageProps} />
+      </CountdownProvider>
     </ChallengesProvider>
   )
 }
