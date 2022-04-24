ec66b7ec5295e1687ed3981a660da833cc5bf6bf
William almeida <willliam.dev@gmail.com>
2022-04-23 22:42:39 -0300

    saving data with cookies: :white_check_mark:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage, GetServerSideProps } from 'next'
+import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { ChallengeBox } from '../components/ChallengeBox'
 import { CompletedChallenges } from '../components/CompletedChallenges'
@@ -9,12 +9,20 @@ import { ChallengesProvider } from '../contexts/ChallengesContext'
 import { CountdownProvider } from '../contexts/CountdownContext'
 import styles from '../styles/pages/Home.module.css'
 
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
-const Home: NextPage = (props) => {
+
+const Home = (props: HomeProps) => {
   return (
-    <ChallengesProvider>
-
-
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
       <div className={styles.container}>
         <Head>
           <title>Moveit</title>
@@ -39,3 +47,17 @@ const Home: NextPage = (props) => {
 }
 
 export default Home
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
+}
